6d2b1aefdbadd653e9d9283ebde0eecfb74663b2
Khenya <abrendakhenya@gmail.com>
2023-10-15 18:02:56 -0400

    clases

--- app/src/test/java/basicWeb/SeleniumTest.java
+++ app/src/test/java/basicWeb/SeleniumTest.java
@@ -1,13 +1,9 @@
 package basicWeb;
 
-
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-import java.time.Duration;
 
 public class SeleniumTest {
     ChromeDriver chrome;
@@ -16,29 +12,12 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver","src/test/resources/chrome/chromedriver.exe");
         chrome = new ChromeDriver();
         chrome.manage().window().maximize();
-
-        chrome.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
         chrome.get("http://todo.ly/");
     }
-//.basic("upbapi@upbapi.com", "12345")
+
     @Test
-    public void verifyLoginTest() throws InterruptedException {
-        // click login button
-        chrome.findElement(By.xpath("//img[@src=\"/Images/design/pagelogin.png\"]")).click();
-        // set email
-        chrome.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxEmail")).sendKeys("upbapi@upbapi.com");
-        // set password
-        chrome.findElement(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword")).sendKeys("12345");
-        // click login
-        chrome.findElement(By.id("ctl00_MainContent_LoginControl1_ButtonLogin")).click();
+    public void verifySomeThing() throws InterruptedException {
         Thread.sleep(5000);
-        // verificar si existe el control del logout
-
-        Assertions.assertTrue((chrome.findElements(By.xpath("//a[text()='Logout']")).size() == 1),
-                "ERROR no se pudo ingresar a la sesion");
-
-        Thread.sleep(5000);
-
     }
 
     @AfterEach
